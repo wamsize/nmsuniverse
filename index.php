f9e56564edf2d9260fbbe822c478b56f1d0e93d9
Samuel Friedman <sifriedm@asu.edu>
2016-04-02 03:03:00 -0700

    Original Commit
    The original site.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>NMS Universe</title>
+		<link rel="stylesheet" href="style.css"/>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	</head>
+	<body>
+		<div class="back" style="position: absolute; top: 0; width: 100%; height: 100%; background-image: url(img/home.jpg); background-size: cover; background-repeat: no-repeat;"></div>
+		<div style="width: 100%; height: 100%; position: absolute; background-color: rgba(0,0,0,0.4);"></div>
+		<div class="home" style="position: absolute; top: 0; width: 100%; height: 100%;">
+			<img src="img/logo.png" style="width: 60%; margin-left: 20%; margin-top: 130px; text-align: center;"/>
+		</div>
+		<div class="content" style="background-color: white; position: absolute; top: 100vh; width: 60%; padding: 40px 20vw;">
+			Welcome to the No Man's Sky Universe website.
+		</div>
+	</body>
+</html>
